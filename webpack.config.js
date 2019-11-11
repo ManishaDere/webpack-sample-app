8b5f6ee15409a8bcf29969e768c378d9f081a3c9
Manisha Dere <manu.pathare263@gmail.com>
2019-11-11 12:33:58 +0530

    webpack file setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack'),
+    path = require('path');
+
+module.exports = {
+	mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader',
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            'file-loader',
+          ],
+        },
+      ]
+  },
+  watch: true
+}
